417e73bcf87fa645b5e471d2b68d87b213eaf006
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-01 15:19:25 +0700

    Основы JavaScript: Написал функцию для вычисления факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,18 @@
 function factorial(n) {
-  // ваш код...
+  if (Number(n) !== n && n % 1 !== 0 || Math.sign(n) === -1) {
+    console.error('factorial: the argument "n" is incorrect');
+    return NaN;
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let result = 1;
+
+  for (let num = 1; num <= n; num++) {
+    result *= num;
+  }
+
+  return result;
 }
