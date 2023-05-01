89a317f31eabd1ca6cfb4dfc036a3345075a1922
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-01 15:34:15 +0700

    Основы JavaScript: Добавил в функцию факториала проверку если аргумент не определен и убрал лишнюю проверку

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,11 +1,7 @@
 function factorial(n) {
-  if (Number(n) !== n && n % 1 !== 0 || Math.sign(n) === -1) {
+  if (n === undefined || Number(n) !== n && n % 1 !== 0 || Math.sign(n) === -1) {
     console.error('factorial: the argument "n" is incorrect');
     return NaN;
-  }
-
-  if (n === 0 || n === 1) {
-    return 1;
   }
 
   let result = 1;
