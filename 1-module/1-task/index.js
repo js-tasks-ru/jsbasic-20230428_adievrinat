20b570a8fb7c3486a1e59ccbdf23b197af7b31b8
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-02 11:54:24 +0700

    Основы JavaScript: factorial - фикс в условии проверки аргумента и в цикле

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,12 +1,12 @@
 function factorial(n) {
-  if (!n || Number(n) !== n && n % 1 !== 0 || Math.sign(n) === -1) {
+  if (!n && n !== 0 || Number(n) !== n && n % 1 !== 0 || Math.sign(n) === -1) {
     console.error('factorial: the "n" argument is incorrect');
     return NaN;
   }
 
   let result = 1;
 
-  for (let num = 1; num <= n; num++) {
+  for (let num = 2; num <= n; num++) {
     result *= num;
   }
 
