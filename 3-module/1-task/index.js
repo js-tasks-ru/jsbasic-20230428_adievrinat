c034c1cca43bb57fa0fe8776a916c80887431416
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-13 22:57:13 +0700

    Занятие 3: Функции и массивы. Решил 3 задачи

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,15 @@
 function namify(users) {
-  // ваш код...
+  if (!users || !Array.isArray(users)) {
+    return users;
+  }
+
+  const names = [];
+
+  for (let user of users) {
+    if (user.hasOwnProperty('name')) {
+      names.push(user.name);
+    }
+  }
+
+  return names;
 }
--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  if (typeof str !== 'string' || !str) {
+    return str;
+  }
+
+  const array = str.split('');
+
+  array.forEach((item, index) => {
+    if (item === '-' && array[index + 1] !== '-') {
+      array[index + 1] = array[index + 1].toUpperCase();
+    }
+  });
+
+  return array.filter(item => item !== '-').join('');
 }
