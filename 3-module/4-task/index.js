4cb5fb1b2ada40fca5005b9153e388950906b092
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-14 19:26:07 +0700

    Занятие 3: Функции и массивы. Решил четвертую задачу

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  if (!Array.isArray(users)) {
+    return users;
+  }
+
+  return users.reduce((prev, item) => {
+    let prevData = (typeof prev !== 'string') && (prev.age <= age) ? `${prev.name}, ${prev.balance}` : prev;
+
+    return item.age <= age ? `${prevData}\n${item.name}, ${item.balance}` : prevData;
+  });
 }
