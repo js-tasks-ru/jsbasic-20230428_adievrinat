daf0dc46a79d6cd72e75229416b113e781ba33f4
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-15 18:08:51 +0700

    Занятие 3: Функции и массивы. Рефакторинг

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -3,9 +3,5 @@ function showSalary(users, age) {
     return users;
   }
 
-  return users.reduce((prev, item) => {
-    let prevData = (typeof prev !== 'string') && (prev.age <= age) ? `${prev.name}, ${prev.balance}` : prev;
-
-    return item.age <= age ? `${prevData}\n${item.name}, ${item.balance}` : prevData;
-  });
+  return users.filter(user => user.age <= age).map(item => `${item.name}, ${item.balance}`).join('\n');
 }
