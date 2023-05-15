32ed5a7dd1b71b934d0552f1321088a1902fe309
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-15 10:06:14 +0700

    Занятие 3: Функции и массивы. Рефакторинг

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -7,11 +7,9 @@ function getMinMax(str) {
   const numbers = arrayFromString.filter(numberForFilter => isFinite(Number(numberForFilter))).map(item => Number(item));
 
   return numbers.reduce((prev, number) => {
-    let firstArg = name => prev[name];
-
     return {
-      min: Math.min(firstArg('min'), number),
-      max: Math.max(firstArg('max'), number)
+      min: Math.min(prev.min, number),
+      max: Math.max(prev.max, number)
     };
   }, {min: numbers[0], max: numbers[0]});
 }
