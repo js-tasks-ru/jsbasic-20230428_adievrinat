c09667e32c24603342ba3497e3f8811984ddc56f
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-15 09:47:35 +0700

    Занятие 3: Функции и массивы. Решил пятую задачу

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  if (typeof str !== 'string') {
+    return str;
+  }
+
+  const arrayFromString = str.split(' ');
+  const numbers = arrayFromString.filter(numberForFilter => isFinite(Number(numberForFilter))).map(item => Number(item));
+
+  return numbers.reduce((prev, number) => {
+    let firstArg = name => typeof prev === 'number' ? prev : prev[name];
+
+    return {
+      min: Math.min(firstArg('min'), number),
+      max: Math.max(firstArg('max'), number)
+    };
+  });
 }
