9386cf6335278fbdacc31760da5eb9a63c577b62
rinatadiev <adiev.rr@sibvaleo.com>
2023-05-30 14:19:45 +0700

    Занятие 6: JS классы и компоненты. Учебный проект: Карточка товара

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,46 @@
+import createElement from "../../assets/lib/create-element";
+
 export default class ProductCard {
   constructor(product) {
+    const check = typeof product === 'object' && !Array.isArray(product) && product !== null;
+    this.elem = check ? this.#createProductCart(product) : product;
+  }
+
+  #createProductCart(product) {
+    const elem = createElement(this.#getTemplate(product));
+    const button = this.#createBtn();
+
+    button.addEventListener('click', function () {
+      elem.dispatchEvent(new CustomEvent("product-add", {
+        detail: product.id,
+        bubbles: true
+      }));
+    });
+
+    elem.querySelector('.card__body').appendChild(button);
+
+    return elem;
+  }
+
+  #createBtn() {
+    const button = document.createElement('BUTTON');
+
+    button.setAttribute('type', 'button');
+    button.classList.add('card__button');
+    button.innerHTML = '<img src="/assets/images/icons/plus-icon.svg" alt="icon">';
+
+    return button;
+  }
+
+  #getTemplate(data) {
+    return `<div class="card">
+      <div class="card__top">
+        <img src="/assets/images/products/${data.image}" class="card__image" alt="product">
+        <span class="card__price">€${data.price.toFixed(2)}</span>
+      </div>
+      <div class="card__body">
+        <div class="card__title">${data.name}</div>
+      </div>
+    </div>`;
   }
 }
