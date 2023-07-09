9b17304b58911acf0a9f9fc8bb357e75b2977207
rinatadiev <adiev.rr@sibvaleo.com>
2023-07-09 12:15:25 +0700

    Промисификация клика по кнопке

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,9 @@
-export default function promiseClick(button) {
-  // ваш код...
+export default async function promiseClick(button) {
+  return new Promise(function(resolve) {
+    button.addEventListener('click', e => {
+      resolve(e);
+    }, {
+      once: true
+    });
+  });
 }
